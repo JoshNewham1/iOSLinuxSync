793a19bf236ec37847609b67f79f143b1aedd5a2
Josh Newham <joshnewham@live.com>
2023-07-01 19:02:55 +0100

    Added option to convert all HEIC files to JPG

--- ios-sync.sh
+++ ios-sync.sh
@@ -30,6 +30,8 @@ if [ -z "$destination_path" ]; then
 	exit 1;
 fi
 
+# TODO check deps
+
 copy_files () {
 	# Read possible Apple devices into an array
 	devices_dir="/run/user/1000/gvfs"
@@ -71,7 +73,36 @@ copy_files () {
 }
 
 convert_files () {
+	# Check heif-convert util is installed
+	if ! command -v heif-convert &> /dev/null
+	then
+	    echo "heif-convert COMMAND could not be found."
+	    echo "Please install 'libheif-examples' first."
+	    echo "To install 'libheif-examples', run the following command:"
+	    echo "  sudo apt install libheif-examples"
+	    exit
+	fi
+    
 	echo "Converting all images from HEIC to JPG...";
+	# Get all HEIC files recursively and store in array
+	readarray -t heic_files < <(find "$destination_path" -type f -iname "*.heic" -printf "%P\n");
+	for file in "${heic_files[@]}";
+	do
+		# Do a case-insensitive replace of .heic with .jpg for the output name
+		output_file_name=`echo $file | sed 's/\.heic/\.jpg/Ig'`;
+		# If the jpg file doesn't already exist
+		if ! [ -e "$output_file_name" ]; then
+			# Convert it
+			heif-convert "$file" "$output_file_name" -q 100;
+
+			# Remove the accompanying HDR map if it exists
+			auxiliary_image_name=$(sed 's/\.jpg/-urn\:com\:apple\:photo\:2020\:aux\:hdrgainmap\.jpg/Ig' <<< $output_file_name);
+			if [ -e "$auxiliary_image_name" ]; then
+				rm $auxiliary_image_name;
+				echo "Removed auxiliary image $auxiliary_image_name...";
+			fi
+		fi
+	done
 }
 
-convert_files
+copy_files
